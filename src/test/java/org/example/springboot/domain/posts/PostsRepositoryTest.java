d0da391f395a3fbdc7c50474b47026ebf10b9b99
Heedong Kim <ruthetum@gmail.com>
2020-10-16 13:11:33 +0900

    [feat] JPA 테스트 코드 작성

---
+++ src/test/java/org/example/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,46 @@
+package org.example.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After // 단위 테스트가 끝날 때마다 수행되는 메소드를 지정
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        // id 값이 있으면 update, 없으면 insert
+        postsRepository.save(Posts.builder()
+                            .title(title)
+                            .content(content)
+                            .author("hd@naver.com")
+                            .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
