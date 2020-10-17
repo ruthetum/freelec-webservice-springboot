7c8353281d7b08d336e2247f3749beb1de53ee68
Heedong Kim <ruthetum@gmail.com>
2020-10-18 02:11:21 +0900

    [feat] 등록된 게시글 조회

--- src/main/java/org/example/springboot/domain/posts/PostsRepository.java
+++ src/main/java/org/example/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,12 @@
 package org.example.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
 // DaO 라고 생각, DB Layer 접근자, interface로 생성, JpaRepository<Entity 클래스, PK> 상속
