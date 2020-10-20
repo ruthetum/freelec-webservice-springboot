81413672d003846a2f6db43af82532197c1b1514
Heedong Kim <ruthetum@gmail.com>
2020-10-20 21:15:34 +0900

    어노테이션 기반으로 코드 수정

--- src/main/java/org/example/springboot/web/IndexController.java
+++ src/main/java/org/example/springboot/web/IndexController.java
@@ -1,6 +1,7 @@
 package org.example.springboot.web;
 
 import lombok.RequiredArgsConstructor;
+import org.example.springboot.config.auth.LoginUser;
 import org.example.springboot.config.auth.dto.SessionUser;
 import org.example.springboot.service.posts.PostsService;
 import org.example.springboot.web.dto.PostsResponseDto;
@@ -18,12 +19,23 @@ public class IndexController {
     private final PostsService postsService;
     private final HttpSession httpSession;
 
+//    @GetMapping("/")
+//    public String index(Model model){
+//        model.addAttribute("posts", postsService.findAllDesc());
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user"); // 로그인 성공 시 세션 값 저장
+//        if (user != null) {
+//            model.addAttribute("userName", user.getName()); // 세션에 저장된 값이 있을 때만 model에 userName으로 등록
+//        }
+//        return "index";
+//    }
+
+    // annotation 기반으로 코드 수정
+    // LoginUser 어노테이션을 추가해서 httpSession.getAttribute("user")를 통해 세션 가져올 필요 없음
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user"); // 로그인 성공 시 세션 값 저장
         if (user != null) {
-            model.addAttribute("userName", user.getName()); // 세션에 저장된 값이 있을 때만 model에 userName으로 등록
+            model.addAttribute("userName", user.getName());
         }
         return "index";
     }
